cf9af9e84087e492e926c8b93b293d0b396c1017
Allan Tavares <allan.tavares@estudante.ufla.br>
2024-03-26 19:16:37 -0300

    first commit

---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,53 @@
+import React from 'react';
+import './Hero.css';
+import Header from '../Header/Header';
+
+const Hero = () => {
+    return (
+        <div className="hero">
+
+            <div className='left-h'>
+                <Header />
+                <div className="the-best-ad">
+                    <div></div>
+                    <span>Bruno Frango xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx </span>
+                </div>
+                <div className="hero-text">
+                    <div>
+                        <span className='stroke-text'>que ficar com a bunda durinha  </span>
+                        <span>igual a minha</span>
+                    </div>
+                    <div>
+                        <span>
+                           Pro verão 
+                        </span>
+                    </div>
+                    <div>
+                    <span>Aqui o processo e bruto, te ensino a ficar com o cuzinho bem apertado pra seu macho ficar louco igual ao meu fica</span>
+                </div>
+                </div>
+
+               <div className="figures">
+                <div>
+                    <span>+140</span>
+                    <span>Alunos </span>
+                </div>
+                <div>
+                    <span>+20</span>
+                    <span>Alunos que eu mamo</span>
+                </div>
+                <div>
+                    <span>+40</span>
+                    <span> Que eu sento</span>
+                </div>
+
+                
+                </div> 
+            
+            </div>
+            <div className='right-h'>Right side</div>
+        </div>
+    );
+}
+
+export default Hero;
